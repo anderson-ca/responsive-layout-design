fa209929188ac34701bb0130616606414bf0bcf4
anderson <anderson.d.cardoso92@gmail.com>
2022-01-31 14:16:14 -0600

    add several row markup and styles

--- pages/FlexboxGridSystem.jsx
+++ pages/FlexboxGridSystem.jsx
@@ -20,14 +20,14 @@ const FlexboxGridSystem = () => {
             <p>lorem ipsum dolor sit amet, consectetur adip</p>
           </div>
         </div>
-        <div className={`${styles.figure} ${styles.span2}`}>
+        <div className={`${styles.figure}`}>
           <div className={styles.three}></div>
           <div className={styles.figcaption}>
             <h2>Abstract 3</h2>
             <p>lorem ipsum dolor sit amet, consectetur adip</p>
           </div>
         </div>
-        <div className={`${styles.figure} ${styles.span2}`}>
+        <div className={`${styles.figure}`}>
           <div className={styles.four}></div>
           <div className={styles.figcaption}>
             <h2>Abstract 4</h2>
@@ -37,94 +37,77 @@ const FlexboxGridSystem = () => {
       </div>
 
       {/* Row 2: One box per column */}
-      <div className={styles.figure}>
-        <div className={styles.one}></div>
-        <div className={styles.figcaption}>
-          <h2>Abstract 1</h2>
-          <p>lorem ipsum dolor sit amet, consectetur adip</p>
+      <div className={styles.row}>
+        <div className={`${styles.figure} ${styles.span2}`}>
+          <div className={styles.one}></div>
+          <div className={styles.figcaption}>
+            <h2>Abstract 3</h2>
+            <p>lorem ipsum dolor sit amet, consectetur adip</p>
+          </div>
         </div>
-      </div>
-      <div className={styles.figure}>
-        <div className={styles.two}></div>
-        <div className={styles.figcaption}>
-          <h2>Abstract 2</h2>
-          <p>lorem ipsum dolor sit amet, consectetur adip</p>
-        </div>
-      </div>
-      <div className={styles.figure}>
-        <div className={styles.three}></div>
-        <div className={styles.figcaption}>
-          <h2>Abstract 3</h2>
-          <p>lorem ipsum dolor sit amet, consectetur adip</p>
-        </div>
-      </div>
-      <div className={styles.figure}>
-        <div className={styles.four}></div>
-        <div className={styles.figcaption}>
-          <h2>Abstract 4</h2>
-          <p>lorem ipsum dolor sit amet, consectetur adip</p>
+        <div className={`${styles.figure} ${styles.span2}`}>
+          <div className={styles.two}></div>
+          <div className={styles.figcaption}>
+            <h2>Abstract 1</h2>
+            <p>lorem ipsum dolor sit amet, consectetur adip</p>
+          </div>
         </div>
       </div>
 
       {/* Row 3: One box per column */}
-      <div className={styles.figure}>
-        <div className={styles.one}></div>
-        <div className={styles.figcaption}>
-          <h2>Abstract 1</h2>
-          <p>lorem ipsum dolor sit amet, consectetur adip</p>
+      <div className={styles.row}>
+        <div className={styles.figure}>
+          <div className={styles.two}></div>
+          <div className={styles.figcaption}>
+            <h2>Abstract 2</h2>
+            <p>lorem ipsum dolor sit amet, consectetur adip</p>
+          </div>
         </div>
-      </div>
-      <div className={styles.figure}>
-        <div className={styles.two}></div>
-        <div className={styles.figcaption}>
-          <h2>Abstract 2</h2>
-          <p>lorem ipsum dolor sit amet, consectetur adip</p>
-        </div>
-      </div>
-      <div className={styles.figure}>
-        <div className={styles.three}></div>
-        <div className={styles.figcaption}>
-          <h2>Abstract 3</h2>
-          <p>lorem ipsum dolor sit amet, consectetur adip</p>
-        </div>
-      </div>
-      <div className={styles.figure}>
-        <div className={styles.four}></div>
-        <div className={styles.figcaption}>
-          <h2>Abstract 4</h2>
-          <p>lorem ipsum dolor sit amet, consectetur adip</p>
+        <div className={styles.figure}>
+          <div className={styles.four}></div>
+          <div className={styles.figcaption}>
+            <h2>Abstract 4</h2>
+            <p>lorem ipsum dolor sit amet, consectetur adip</p>
+          </div>
         </div>
       </div>
 
       {/* Row 4: One box per column */}
-      <div className={styles.figure}>
-        <div className={styles.one}></div>
-        <div className={styles.figcaption}>
-          <h2>Abstract 1</h2>
-          <p>lorem ipsum dolor sit amet, consectetur adip</p>
+      <div className={styles.row}>
+        <div className={styles.figure}>
+          <div className={styles.four}></div>
+          <div className={styles.figcaption}>
+            <h2>Abstract 4</h2>
+            <p>lorem ipsum dolor sit amet, consectetur adip</p>
+          </div>
         </div>
       </div>
-      <div className={styles.figure}>
-        <div className={styles.two}></div>
-        <div className={styles.figcaption}>
-          <h2>Abstract 2</h2>
-          <p>lorem ipsum dolor sit amet, consectetur adip</p>
+
+      {/* Row 5: 3 boxes, 1-2-1 distribution */}
+      <div className={styles.row}>
+        <div className={styles.figure}>
+          <div className={styles.five}></div>
+          <div className={styles.figcaption}>
+            <h2>Abstract 4</h2>
+            <p>lorem ipsum dolor sit amet, consectetur adip</p>
+          </div>
+        </div>
+        <div className={styles.figure}>
+          <div className={styles.five}></div>
+          <div className={styles.figcaption}>
+            <h2>Abstract 2</h2>
+            <p>lorem ipsum dolor sit amet, consectetur adip</p>
+          </div>
+        </div>
+        <div className={styles.figure}>
+          <div className={styles.five}></div>
+          <div className={styles.figcaption}>
+            <h2>Abstract 1</h2>
+            <p>lorem ipsum dolor sit amet, consectetur adip</p>
+          </div>
         </div>
       </div>
-      <div className={styles.figure}>
-        <div className={styles.three}></div>
-        <div className={styles.figcaption}>
-          <h2>Abstract 3</h2>
-          <p>lorem ipsum dolor sit amet, consectetur adip</p>
-        </div>
-      </div>
-      <div className={styles.figure}>
-        <div className={styles.four}></div>
-        <div className={styles.figcaption}>
-          <h2>Abstract 4</h2>
-          <p>lorem ipsum dolor sit amet, consectetur adip</p>
-        </div>
-      </div>
+
     </div>
   );
 };
