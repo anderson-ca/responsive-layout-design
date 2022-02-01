8f73bf6cc1dbc1e44f6606fbc059507ce59f4d6c
anderson <anderson.d.cardoso92@gmail.com>
2022-02-01 09:47:54 -0600

    finish basic grid system using flex box rows/columns

--- pages/FlexboxGridSystem.jsx
+++ pages/FlexboxGridSystem.jsx
@@ -39,14 +39,14 @@ const FlexboxGridSystem = () => {
       {/* Row 2: One box per column */}
       <div className={styles.row}>
         <div className={`${styles.figure} ${styles.span2}`}>
-          <div className={styles.one}></div>
+          <div className={styles.three}></div>
           <div className={styles.figcaption}>
             <h2>Abstract 3</h2>
             <p>lorem ipsum dolor sit amet, consectetur adip</p>
           </div>
         </div>
         <div className={`${styles.figure} ${styles.span2}`}>
-          <div className={styles.two}></div>
+          <div className={styles.one}></div>
           <div className={styles.figcaption}>
             <h2>Abstract 1</h2>
             <p>lorem ipsum dolor sit amet, consectetur adip</p>
@@ -85,22 +85,22 @@ const FlexboxGridSystem = () => {
 
       {/* Row 5: 3 boxes, 1-2-1 distribution */}
       <div className={styles.row}>
-        <div className={`${styles.figure} ${styles.span2}`}>
-          <div className={styles.five}></div>
+        <div className={`${styles.figure} ${styles.span2_4}`}>
+          <div className={styles.four}></div>
           <div className={styles.figcaption}>
             <h2>Abstract 4</h2>
             <p>lorem ipsum dolor sit amet, consectetur adip</p>
           </div>
         </div>
         <div className={`${styles.figure} ${styles.order_1}`}>
-          <div className={styles.five}></div>
+          <div className={styles.two}></div>
           <div className={styles.figcaption}>
             <h2>Abstract 2</h2>
             <p>lorem ipsum dolor sit amet, consectetur adip</p>
           </div>
         </div>
         <div className={`${styles.figure} ${styles.order_3}`}>
-          <div className={styles.five}></div>
+          <div className={styles.one}></div>
           <div className={styles.figcaption}>
             <h2>Abstract 1</h2>
             <p>lorem ipsum dolor sit amet, consectetur adip</p>
