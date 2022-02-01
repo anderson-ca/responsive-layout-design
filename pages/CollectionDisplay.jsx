74ca79c2d8600f857d8aeb59570ec3f052ec6686
anderson <anderson.d.cardoso92@gmail.com>
2022-02-01 15:54:43 -0600

    add base style to collections exercise

--- pages/CollectionDisplay.jsx
+++ pages/CollectionDisplay.jsx
@@ -10,82 +10,84 @@ import abstract from '../public/abstract.jpg';
 
 const CollectionDisplay = () => {
     return (
-        <div className={`${styles.section}`}>
-            <h2>Collections</h2>
-            <ul>
-                <li>
-                    <div className={`${styles.figure}`}>
-                        <div className={`${styles.imgContainer}`}>
-                            <Image src={popart} alt='pop-art' />
+        <div className={`${styles.body}`}>
+            <div className={`${styles.section}`}>
+                <h2>Collections</h2>
+                <ul>
+                    <li>
+                        <div className={`${styles.figure}`}>
+                            <div className={`${styles.imgContainer}`}>
+                                <Image src={popart} alt='pop-art' />
+                            </div>
+                            <div className={`${styles.figCaption}`}>
+                                <Link href='#' passHref>
+                                    Pop Art
+                                </Link >
+                            </div>
                         </div>
-                        <div className={`${styles.figCaption}`}>
-                            <Link href='#' passHref>
-                                Pop Art
-                            </Link >
+                    </li>
+                    <li>
+                        <div className={`${styles.figure}`}>
+                            <div className={`${styles.imgContainer}`}>
+                                <Image src={abstract} alt='abstract' />
+                            </div>
+                            <div className={`${styles.figCaption}`}>
+                                <Link href='#' passHref>
+                                    Abstract
+                                </Link >
+                            </div>
                         </div>
-                    </div>
-                </li>
-                <li>
-                    <div className={`${styles.figure}`}>
-                        <div className={`${styles.imgContainer}`}>
-                            <Image src={abstract} alt='abstract' />
+                    </li>
+                    <li>
+                        <div className={`${styles.figure}`}>
+                            <div className={`${styles.imgContainer}`}>
+                                <Image src={vivid} alt='vivid' />
+                            </div>
+                            <div className={`${styles.figCaption}`}>
+                                <Link href='#' passHref>
+                                    Vivid
+                                </Link >
+                            </div>
                         </div>
-                        <div className={`${styles.figCaption}`}>
-                            <Link href='#' passHref>
-                                Abstract
-                            </Link >
+                    </li>
+                    <li>
+                        <div className={`${styles.figure}`}>
+                            <div className={`${styles.imgContainer}`}>
+                                <Image src={music} alt='music' />
+                            </div>
+                            <div className={`${styles.figCaption}`}>
+                                <Link href='#' passHref>
+                                    Music
+                                </Link >
+                            </div>
                         </div>
-                    </div>
-                </li>
-                <li>
-                    <div className={`${styles.figure}`}>
-                        <div className={`${styles.imgContainer}`}>
-                            <Image src={vivid} alt='vivid' />
+                    </li>
+                    <li>
+                        <div className={`${styles.figure}`}>
+                            <div className={`${styles.imgContainer}`}>
+                                <Image src={vehicles} alt='vehicles' />
+                            </div>
+                            <div className={`${styles.figCaption}`}>
+                                <Link href='#' passHref>
+                                    Vehicles
+                                </Link >
+                            </div>
                         </div>
-                        <div className={`${styles.figCaption}`}>
-                            <Link href='#' passHref>
-                                Vivid
-                            </Link >
+                    </li>
+                    <li>
+                        <div className={`${styles.figure}`}>
+                            <div className={`${styles.imgContainer}`}>
+                                <Image src={landscape} alt='landscape' />
+                            </div>
+                            <div className={`${styles.figCaption}`}>
+                                <Link href='#' passHref>
+                                    Landscape
+                                </Link >
+                            </div>
                         </div>
-                    </div>
-                </li>
-                <li>
-                    <div className={`${styles.figure}`}>
-                        <div className={`${styles.imgContainer}`}>
-                            <Image src={music} alt='music' />
-                        </div>
-                        <div className={`${styles.figCaption}`}>
-                            <Link href='#' passHref>
-                                Music
-                            </Link >
-                        </div>
-                    </div>
-                </li>
-                <li>
-                    <div className={`${styles.figure}`}>
-                        <div className={`${styles.imgContainer}`}>
-                            <Image src={vehicles} alt='vehicles' />
-                        </div>
-                        <div className={`${styles.figCaption}`}>
-                            <Link href='#' passHref>
-                                Vehicles
-                            </Link >
-                        </div>
-                    </div>
-                </li>
-                <li>
-                    <div className={`${styles.figure}`}>
-                        <div className={`${styles.imgContainer}`}>
-                            <Image src={landscape} alt='landscape' />
-                        </div>
-                        <div className={`${styles.figCaption}`}>
-                            <Link href='#' passHref>
-                                Landscape
-                            </Link >
-                        </div>
-                    </div>
-                </li>
-            </ul>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 };
